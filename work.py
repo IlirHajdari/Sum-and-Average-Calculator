a3ccd647a71a9a8eb762ec2cc9fd06d456ee90c8
Ilir Hajdari <ilir.hajdari111@gmail.com>
2022-05-06 00:21:00 -0400

    Add files via upload

---
+++ work.py
@@ -0,0 +1,15 @@
+# user number inputs
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the second number: "))
+
+# find sum
+sum = (num1 + num2)
+
+# find average
+average = (num1 +num2) / 2
+
+# print sum
+print("The sum of", (num1), "and", (num2), "is:", sum)
+
+# print average
+print("The average of", (num1), "and", (num2), "is: %0.2f" %average)
